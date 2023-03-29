fc4921d775bbf6da4db430597ea11461d0e78d4b
Lizaveta Valynets <volynets2000@mail.ru>
2023-03-29 18:08:04 +0300

    done

---
+++ task13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+bool i = true;
+
+while (i)
+{
+    if (num > 99 && num < 1000)
+    {
+        int thirthDigit = num % 10;
+        Console.WriteLine("Третья цифра: " + thirthDigit);
+        i = false;
+    }
+    else if (num >= 1000)
+        num = num / 10;
+    else if (num <= 99)
+    {
+        Console.WriteLine("Третьей цифры нет.");
+        i = false;
+    }
+}
