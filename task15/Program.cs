9a9abf695f717f6d9341e55cc6a4c9de61dcd0bd
Lizaveta Valynets <volynets2000@mail.ru>
2023-03-29 18:08:23 +0300

    done

---
+++ task15/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число: ");
+bool i = true;
+
+while (i)
+{
+    int num = int.Parse(Console.ReadLine()!); 
+    {
+    if (num == 6 || num == 7)
+    {
+        Console.WriteLine($"День {num} является выходным");
+        i = false; //не ищет следующий такой же элемент
+    }
+    else if (num <= 5 && num >= 1)
+    {   Console.WriteLine($"{num} - это будний день");
+        i = false;
+    }
+    else
+        Console.WriteLine($"{num} - не является днем недели, введите число еще раз");
+    }
+
+}
